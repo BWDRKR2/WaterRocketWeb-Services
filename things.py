7c328579f19c795a61ef3e243c78797ec2cb1a5a
Dan Kozlowski <dankozlowski@sbcglobal.net>
2016-08-19 14:05:17 +0000

    Object class changes

--- things.py
+++ things.py
@@ -1,28 +1,25 @@
-# things.py
-
-
-# Let's get this party started!
 import falcon
 
 
-# Falcon follows the REST architectural style, meaning (among
-# other things) that you think in terms of resources and state
-# transitions, which map to HTTP verbs.
-class ThingsResource(object):
+class Reset(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
         resp.body = ('\nTwo things awe me most, the starry sky '
                      'above me and the moral law within me.\n'
                      '\n'
-                     '    ~ Immanuel Kant\n\n')
+                     '    ~ Dan Immanuel Kant\n\n')
 
-# falcon.API instances are callable WSGI apps
+
 app = falcon.API()
 
-# Resources are represented by long-lived class instances
-things = ThingsResource()
+reset = Reset()
 
-# things will handle all requests to the '/things' URL path
-app.add_route('/things', things)
-app.add_route('/launch', things)
+app.add_route('/things', reset)
+app.add_route('/reset', reset)
+app.add_route('/arm', reset)
+app.add_route('/unarm', reset)
+app.add_route('/begin_fuel', reset)
+app.add_route('/end_fuel', reset)
+app.add_route('/abort', reset)
+app.add_route('/launch', reset)
