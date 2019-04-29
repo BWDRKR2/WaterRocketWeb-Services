7ecf98e56d61ff1efb24ebd377ddf5cd0c9678cc
Dan Kozlowski <BWDRKR2@mainoffice.localdomain>
2019-04-29 16:32:48 -0500

    cleanup

--- Restful.py
+++ Restful.py
@@ -1,14 +1,6 @@
 import falcon
 import launch_api
 
-class Reset(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = ('\nTwo things awe me most, the starry sky '
-                     'above me and the moral law within me.\n'
-                     '\n'
-                     '    ~ Dan Immanuel Kant\n\n')
 
 class Arm(object):
     def on_get(self, req, resp):
@@ -23,17 +15,17 @@ class Unarm(object):
 	#a=launch_api.unarm()
 
 class BeginFuel(object):
-    def on_get(self, req, resp):
-	a=launch_api.beginfuel()
+    def on_get(self, req, resp, fuel_amount):
+        a=launch_api.beginfuel(fuel_amount)
 	resp.status = falcon.HTTP_200
 	resp.body = ('Begin Fuel = Y')
 	
 
 class EndFuel(object):
     def on_get(self, req, resp):
+        a=launch_api.endfuel()
 	resp.status = falcon.HTTP_200
-	resp.body = ('End Fuel = Y')
-	a=launch_api.endfuel()	
+	resp.body = ('End Fuel = Y')	
 
 class Abort(object):
     def on_get(self, reg, resp):
@@ -42,13 +34,14 @@ class Abort(object):
 
 class Launch(object):
     def on_get(self, reg, resp):
+        a=launch_api.launch()
 	resp.status = falcon.HTTP_200
 	resp.body = ('Launch = Y')
 	resp.body = ('Lift Off')
 
 app = falcon.API()
 
-reset = Reset()
+
 arm = Arm()
 unarm = Unarm()
 beginfuel = BeginFuel()
@@ -56,10 +49,10 @@ endfuel = EndFuel()
 abort = Abort()
 launch = Launch()
 
-app.add_route('/reset', reset)
+
 app.add_route('/arm', arm)
 app.add_route('/unarm', unarm)
-app.add_route('/begin_fuel', beginfuel)
+app.add_route('/begin_fuel/{fuel_amount}', beginfuel)
 app.add_route('/end_fuel', endfuel)
 app.add_route('/abort', abort)
 app.add_route('/launch', launch)
--- launch_api.py
+++ launch_api.py
@@ -28,10 +28,11 @@ def unarm():
 	fueling = 'N'
 	fueled = 'N'
 	launch = 'N'
-	createconfig(armed, fueling, fueled, launch)
+	createconfig(armed, fueling, fueled, launch, abort)
 	return armed
 
-def beginfuel():
+def beginfuel(fuel_amount):
+        fuel_amount = int(fuel_amount)
 	pfr = pifacerelayplus.PiFaceRelayPlus(pifacerelayplus.RELAY)	
 	print('Begin Fueling')
 	armed = 'Y'
@@ -39,13 +40,16 @@ def beginfuel():
 	fueled = 'N'
 	launch = 'N'
 	abort = 'N'
+        pfr.relays[0].turn_on()
+        sleep(fuel_amount) 
+        pfr.relays[0].toggle()
 	createconfig(armed, fueling, fueled, launch, abort) 
-	pfr.relays[0].turn_on()
-	sleep(10)
-	endfuel(pfr)
 
-def endfuel(pfr):
-	#pfr = pifacerelayplus.PiFaceRelayPlus(pifacerelayplus.RELAY)
+
+
+
+def endfuel():
+	pfr = pifacerelayplus.PiFaceRelayPlus(pifacerelayplus.RELAY)
 	print('End Fueling')
 	armed = 'Y'
 	fueling = 'N'
@@ -66,27 +70,29 @@ def abort():
 	createconfig(armed, fueling, fueled, launch, abort)
 
 
-def launch(pfr):
+def launch():
 	armed, fueling, fueled, launch, abort = readconfig()
+        pfr = pifacerelayplus.PiFaceRelayPlus(pifacerelayplus.RELAY)
+	##sleep(2)
+	##pfr.relays[1].toggle()
 
-	if armed == 'Y' and fueled == 'Y' and abort == 'N'  :
-		count = 10
-		print('Starting Launching Squence')
-		while (count !=  0 and abort == 'N'):
-			print(count)
-			sleep(1)
-			count = count - 1
-			armed, fueling, fueled, launch, abort = readconfig()			
+        ##
+        pfr.relays[0].turn_on()
+        ##
+ 
+        pfr.relays[1].turn_on()
+        sleep(1) 
+        pfr.relays[1].toggle()
+
+        ##
+        pfr.relays[0].toggle()
+        ## 
 
 
-		pfr.relays[1].turn_on()
-		print('Lift Off')
-		sleep(2)
-		pfr.relays[1].toggle()
-		fueling = 'N'
-		launch = 'Y'
-		abort = 'N'
-		createconfig(armed, fueling, fueled, launch, abort)
+	fueling = 'N'
+	launch = 'Y'
+	abort = 'N'
+	createconfig(armed, fueling, fueled, launch, abort)
 
 def createconfig(Armed, Fueling, Fueled, Launch, Abort):
 	cfgfile = open("launch.cfg",'w')
